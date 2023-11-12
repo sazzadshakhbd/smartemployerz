56e3dfcf24e777151494946e11d42f9e2cb5b957
netcodensazzadali <netcodensazzadali@gmail.com>
2023-11-12 09:02:14 +0600

    project initialize

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php @include("./view/header.php") ?>
+
+<body>
+    
+    <?php @include("./view/navbar.php") ?>
+    <!-- Start Main Section -->
+    <main>
+        
+    </main>
+    <!-- End Main Section -->
+
+    <?php @include("./view/footer.php") ?>
+
+    
+
+</body>
+
+</html>
---
+++ view/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="./img/logo/favicon.ico" title="smartemployerz" sizes="32x32">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="smartemployerz">
+
+    <!-- Bootstrap CSS  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--  Font-Awesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./css/style.css" />
+
+    <title>
+        smartemployerz
+    </title>
+
+</head>
